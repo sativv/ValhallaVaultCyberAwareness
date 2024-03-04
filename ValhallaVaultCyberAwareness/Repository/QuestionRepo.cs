d31917d34685164b68b32c2684b2e4c8fb26e0d7
David Vitas <111963497+sativv@users.noreply.github.com>
2024-03-04 11:36:55 +0100

    adding repositories and pages

--- ValhallaVaultCyberAwareness/Repository/QuestionRepo.cs
+++ ValhallaVaultCyberAwareness/Repository/QuestionRepo.cs
@@ -1,6 +1,14 @@
-﻿namespace ValhallaVaultCyberAwareness.Repository
+﻿using ValhallaVaultCyberAwareness.Data;
+
+namespace ValhallaVaultCyberAwareness.Repository
 {
     public class QuestionRepo
     {
+        private readonly ApplicationDbContext context;
+
+        public QuestionRepo(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
     }
 }
