720c146d33f3eaa1ddec2fe841a016a9aceddce9
David Vitas <david.vitas@hotmail.com>
2024-03-05 14:12:13 +0100

    removing subcategory and seeding data

--- ValhallaVaultCyberAwareness/Models/SubCategoryModel.cs
+++ ValhallaVaultCyberAwareness/Models/SubCategoryModel.cs
@@ -2,14 +2,14 @@
 
 namespace ValhallaVaultCyberAwareness.Models
 {
-    public class SubCategoryModel
-    {
-        [Key]
-        public int Id { get; set; }
-        public string Name { get; set; } = null!;
-        public string? InfoText { get; set; }
-        public int SegmentId { get; set; }
-        public SegmentModel Segment { get; set; } = null!;
-        public List<QuestionModel> Questions { get; set; } = new();
-    }
+    //public class SubCategoryModel
+    //{
+    //    [Key]
+    //    public int Id { get; set; }
+    //    public string Name { get; set; } = null!;
+    //    public string? InfoText { get; set; }
+    //    public int SegmentId { get; set; }
+    //    public SegmentModel Segment { get; set; } = null!;
+    //    public List<QuestionModel> Questions { get; set; } = new();
+    //}
 }
