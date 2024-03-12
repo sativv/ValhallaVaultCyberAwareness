2aaa279f871a38472c0ccdfb8bf8803a61d80e00
Oguzcan Avci <Oguzcan.avci95@gmail.com>
2024-03-12 11:35:51 +0100

    Add some infotext about all segments

---
+++ ValhallaVaultCyberAwareness/Data/Migrations/20240312093401_SeedSegment1InfoText.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ValhallaVaultCyberAwareness.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedSegment1InfoText : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Segments",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "InfoText",
+                value: "Bedrägerier är handlingar där en person eller grupp försöker vilseleda, lura eller bedranågon annan för personlig vinning. Det kan omfatta olika platformar samt olika metoder som exempelvisinternet, telefon, e-post och personlig interaktion.");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Segments",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "InfoText",
+                value: null);
+        }
+    }
+}
