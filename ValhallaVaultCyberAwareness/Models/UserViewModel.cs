d0bc7b71d34887216867f10ad83646bb741d8561
OttJoh94 <olg.johansson@gmail.com>
2024-03-12 14:37:50 +0100

    cleanup

--- ValhallaVaultCyberAwareness/Models/UserViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ValhallaVaultCyberAwareness.Models
-{
-	public class UserViewModel
-	{
-		public string Id { get; set; }
-		public string Username { get; set; }
-		public double Category { get; set; }
-	}
-}
